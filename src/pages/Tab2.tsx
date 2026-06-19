3c0d4db126b4cd7301aef85abaad281e71f184f3
ChantalMorales <chantyn7000@gmail.com>
2026-06-18 19:00:32 -0700

    metodo post terminado

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,47 @@
+import { useHistory } from 'react-router';
 import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-
 import './Tab2.css';
+import { RepositoryPayload } from '../interfaces/RepositoryPayload';
+import { createRepository } from '../services/GithubService';
+import { useState } from 'react';
+import LoadingSpinner from '../components/LoadingSpinner';
+
 
 const Tab2: React.FC = () => {
+  const history = useHistory();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const repoFormData: RepositoryPayload = {
+    name: '',
+    description: '',
+  };
+
+  const setFormName = (value: string) => {
+    repoFormData.name = value;
+  };
+
+  const setFormDescription = (value: string) => {
+    repoFormData.description = value;
+  };
+
+  const saveRepository = async () =>{
+    if(repoFormData.name.trim() === ''){
+      alert('El nombre del repositorio es requerido');
+      return;
+    }
+    setIsLoading(true);
+    createRepository(repoFormData).then((newRepo) =>{
+      if(newRepo){
+        history.push('/tab1');
+      }
+    }).catch((error) =>{
+      setIsLoading(false);
+      console.error('Error al crear el repositorio:', error);
+      alert('Error al crear el repositorio');
+    }).finally(() =>{
+      setIsLoading(false);
+    });
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -22,20 +61,28 @@ const Tab2: React.FC = () => {
             labelPlacement="floating"
             className="form-field"
             placeholder="Ingrese el nombre del repositorio"
+            value={repoFormData.name}
+            onIonChange={(e) => setFormName(e.detail.value!)}
           />
           <IonInput
             label="Descripción"
             labelPlacement="floating"
             className="form-field"
             placeholder="Ingrese una descripción breve"
+            value={repoFormData.description}
+            onIonChange={(e) => setFormDescription(e.detail.value!)}
           />
           <IonButton
             className="form-field"
             expand="block"
-            fill="solid" >
-            Agregar Repositorio
+            fill="solid"
+            onClick={saveRepository}  
+            disabled={isLoading}
+          >
+            Guardar Repositorio
           </IonButton>
         </div>
+        <LoadingSpinner isOpen={isLoading} />
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,7 +1,25 @@
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from '@ionic/react';
 import './Tab3.css';
+import { useState } from 'react';
+import { getUserInfo } from '../services/GithubService';
+import { GithubUser } from '../interfaces/GithubUser';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 const Tab3: React.FC = () => {
+  const [userInfo, setUserInfo] = useState<GithubUser | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loadUserInfo = async () => {
+    setIsLoading(true);
+    const userData = await getUserInfo();
+    setUserInfo(userData);
+    setIsLoading(false);
+  }
+
+  useIonViewWillEnter(() => {
+    loadUserInfo();
+  });
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,16 +35,17 @@ const Tab3: React.FC = () => {
         </IonHeader>
         <div className="card-container">
           <IonCard className="profile-card">
-            <img src="https://avatars.githubusercontent.com/u/1?v=4" alt="Avatar" className="profile-avatar" />
+            <img src={userInfo?.avatar_url} alt="Avatar" className="profile-avatar" />
             <IonCardHeader className="profile-card-header">
-              <IonCardTitle>Chantal Morales</IonCardTitle>
-              <IonCardSubtitle>chantalmoralesr</IonCardSubtitle>
+              <IonCardTitle>{userInfo?.name}</IonCardTitle>
+              <IonCardSubtitle>{userInfo?.login}</IonCardSubtitle>
             </IonCardHeader>
             <IonCardContent>
-              <p>desarrolladora de aplicaciones móviles</p>
+              <p>{userInfo?.bio}</p>
             </IonCardContent>
           </IonCard>
         </div>
+        <LoadingSpinner isOpen={isLoading} />
       </IonContent>
     </IonPage>
   );
